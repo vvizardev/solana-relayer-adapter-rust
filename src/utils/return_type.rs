aa4e033f39f318622c1508e1ce15e3c8feb49ca7
vvizardev <nakao95911@gmail.com>
2025-07-24 06:12:14 +0000

    fixL Json RPC Return

--- src/utils/return_type.rs
+++ src/utils/return_type.rs
@@ -4,13 +4,13 @@ use serde_json;
 #[derive(Debug, Deserialize)]
 pub struct JsonRpcResponse {
     pub jsonrpc: String,
-    pub id: String,
+    pub id: u32,
     pub result: Option<String>,
     pub error: Option<JsonRpcError>,
 }
 
 #[derive(Debug, Deserialize)]
 pub struct JsonRpcError {
-    pub code: String,
+    pub code: i32,
     pub message: String,
 }
