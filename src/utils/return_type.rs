ccf55dc6281442d1c6964ad3abf5057bcbd85dad
vvizardev <nakao95911@gmail.com>
2025-07-24 05:41:26 +0000

    update : JsonRpcResponse

--- src/utils/return_type.rs
+++ src/utils/return_type.rs
@@ -4,13 +4,13 @@ use serde_json;
 #[derive(Debug, Deserialize)]
 pub struct JsonRpcResponse {
     pub jsonrpc: String,
-    pub id: u32,
+    pub id: String,
     pub result: Option<String>,
     pub error: Option<JsonRpcError>,
 }
 
 #[derive(Debug, Deserialize)]
 pub struct JsonRpcError {
-    pub code: i32,
+    pub code: String,
     pub message: String,
 }
